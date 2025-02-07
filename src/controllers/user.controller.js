75acb0d1ecdfd1c4bd8ba7d12538076114e5f318
Jean Dresbach <jean.rafael.dresbach@gmail.com>
2025-02-07 08:50:40 -0300

    prisma connecetion create and user routes controller and service added

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,55 @@
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+
+import { UserService } from "../services/user.service"
+
+const userService = new UserService()
+
+export class UserController {
+    async signup(request, response) {
+        try {
+            const { name, email, password } = request.body
+            const hashedPassword = await bcrypt.hash(password, 10)
+            const result = await userService.create({
+                name,
+                email,
+                hashedPassword,
+            })
+
+            return response.status(result.code).json(result)
+        } catch (error) {
+            console.log(error)
+
+            return response.status(500).json({
+                message: "Erro interno do servidor.",
+            })
+        }
+    }
+
+    async login(request, response) {
+        try {
+            const { email, password, stayLoggedIn } = request.body
+
+            const result = await userService.findByEmail(email)
+
+            if (!user || !(await bcrypt.compare(password, user.password))) {
+                return res
+                    .status(401)
+                    .json({ code: 401, message: "Credenciais inválidas." })
+            }
+
+            const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+                expiresIn: stayLoggedIn ? "7d" : process.env.JWT_EXPIRATION,
+            })
+
+            response.json({ token })
+        } catch (error) {
+            console.log(error)
+
+            return response.status(500).json({
+                code: 500,
+                message: `Erro interno do servidor.`,
+            })
+        }
+    }
+}
